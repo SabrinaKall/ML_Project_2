cfa59313875042b3a3f2fdfaa34c042324313997
SabrinaKall <sabrina.ckall@gmail.com>
2018-12-09 18:21:14 +0000

    fixed testGenerator

--- U-net/data.py
+++ U-net/data.py
@@ -85,7 +85,7 @@ def trainGenerator(batch_size,train_path,image_folder,mask_folder,aug_dict,image
 def testGenerator(test_path,num_image = 30,target_size = (400,400),flag_multi_class = False,as_gray = True):
     for i in range(1, num_image + 1):
         img = io.imread(os.path.join(test_path,"test_%d.png"%i),as_gray = as_gray)
-        img = img / 255
+        #img = img / 255
         img = trans.resize(img,target_size)
         img = np.reshape(img,img.shape+(1,)) if (not flag_multi_class) else img
         img = np.reshape(img,(1,)+img.shape)
@@ -119,6 +119,6 @@ def labelVisualize(num_class,color_dict,img):
 
 
 def saveResult(save_path,npyfile,flag_multi_class = False,num_class = 2):
-    for i,item in enumerate(npyfile):
+    for i,item in enumerate(npyfile, 1):
         img = labelVisualize(num_class,COLOR_DICT,item) if flag_multi_class else item[:,:,0]
-        io.imsave(os.path.join(save_path,"prediction_0%d.png"%i),img)
+        io.imsave(os.path.join(save_path,"%d_predict.png"%i),img)
